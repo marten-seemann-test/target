e1ce8c317b52b2587dd546386c4cafcce25b1719
Marten Seemann <martenseemann@gmail.com>
2021-08-04 09:57:05 +0200

    make it a library

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("hello")
-	fmt.Println("bye")
-}
+package target
 
 func mult(a, b int) int {
 	return a * b
 }
 
 func Add(a, b int) int {
-  return a+b
+	return a + b
 }
