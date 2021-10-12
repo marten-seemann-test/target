da204051b51c570d4ebaf9338a1b73d06e470479
Marten Seemann <martenseemann@gmail.com>
2021-10-12 16:30:16 +0200

    add a Div function, use require

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ func mult(a, b int) int {
 	return a * b
 }
 
-func Add(a, b int) int {
-	return a + b
+func Add(a, b, c int) int {
+	return a + b + c
+}
+
+func Div(a, b int) int {
+	return a / b
 }
--- main_test.go
+++ main_test.go
@@ -5,15 +5,21 @@ import (
 	"math/rand"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestRandomSleep(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
-	d := time.Duration(10+rand.Intn(10)) * time.Second
+	d := time.Duration(1+rand.Intn(3)) * time.Second
 	fmt.Println("Sleeping for", d)
 	time.Sleep(d)
 }
 
 func TestMult(t *testing.T) {
-	_ = mult(3, 4)
+	require.Equal(t, mult(2, 3), 6)
+}
+
+func TestAdd(t *testing.T) {
+	require.Equal(t, Add(3, 4, 5), 12)
 }
